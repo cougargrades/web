d549470ef4e24527d81cec1d3256c940c0cce311
Austin Jackson <themacphage@gmail.com>
2019-04-30 08:10:15 +0000

    vibora bad

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
-from vibora import Vibora, Request
-from vibora.responses import JsonResponse
+# examples/server_simple.py
+from aiohttp import web
 
-app = Vibora()
+async def handle(request):
+    name = request.match_info.get('name', "Anonymous")
+    text = "Hello, " + name
+    return web.Response(text=text)
+
+async def wshandle(request):
+    ws = web.WebSocketResponse()
+    await ws.prepare(request)
+
+    async for msg in ws:
+        if msg.type == web.WSMsgType.text:
+            await ws.send_str("Hello, {}".format(msg.data))
+        elif msg.type == web.WSMsgType.binary:
+            await ws.send_bytes(msg.data)
+        elif msg.type == web.WSMsgType.close:
+            break
+
+    return ws
 
 
-@app.route('/')
-async def home(request: Request):
-    return JsonResponse({'hello': 'world'})
+app = web.Application()
+app.add_routes([web.get('/', handle),
+                web.get('/echo', wshandle),
+                web.get('/{name}', handle)])
 
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8000)
+web.run_app(app)
