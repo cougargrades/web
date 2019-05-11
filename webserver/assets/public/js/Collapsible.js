c8bd180dcdace04bb128c2c16f165c88591015d1
Austin Jackson <themacphage@gmail.com>
2019-05-10 23:32:20 -0500

    cleanup

--- webserver/assets/public/js/Collapsible.js
+++ webserver/assets/public/js/Collapsible.js
@@ -1,4 +1,5 @@
 
+// heavily inspired by: https://www.w3schools.com/howto/howto_js_collapsible.asp
 class Collapsible {
     constructor(baseurl, course, container) {
         if(course instanceof Course && container instanceof HTMLElement) {
@@ -24,20 +25,6 @@ class Collapsible {
             //
         }
     }
-
-    // var coll = document.getElementsByClassName("collapsible");
-    // var i;
-    
-    // for (i = 0; i < coll.length; i++) {
-    //   coll[i].addEventListener("click", function() {
-    //     this.classList.toggle("active");
-    //     var content = this.nextElementSibling;
-    //     if (content.style.maxHeight){
-    //       content.style.maxHeight = null;
-    //     } else {
-    //       content.style.maxHeight = content.scrollHeight + "px";
-    //     } 
-    //   });
 
     async createCollapsible(visuals) {
         let button = document.createElement('button')
