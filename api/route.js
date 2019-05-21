0c926d85c73db9647e4c2ca9d8e72a36c923c776
Austin Jackson <themacphage@gmail.com>
2019-05-20 21:07:48 -0500

    implemented mariadb querying

--- api/route.js
+++ api/route.js
@@ -1,13 +1,8 @@
 
 const REVISION = new String(process.env.SOURCE_COMMIT).substring(0,7)
 
-const BASEURL = process.env.BASEURL
-const db = require('better-sqlite3')('records.db', {
-    readonly: true,
-    fileMustExist: true
-});
-
 const timer = require('./lib/timer')
+const db = require('./lib/db')
 const chalk = require('chalk')
 const log = async (pretty, duration, request) => {
     let color = 'green'
@@ -35,55 +30,55 @@ async function routes (fastify, options) {
 
     fastify.get('/api/catalog/list/terms', timer(async (request, reply) => {
         reply.type('application/json')
-        return db.prepare('SELECT DISTINCT TERM_CODE FROM records ORDER BY TERM_CODE ASC').all().map(a => a['TERM_CODE'])
+        return (await db.query('SELECT DISTINCT TERM_CODE FROM records ORDER BY TERM_CODE ASC')).map(a => a['TERM_CODE'])
     }, log))
 
     fastify.get('/api/catalog/list/depts', timer(async (request, reply) => {
         reply.type('application/json')
-        return db.prepare('SELECT DISTINCT DEPT FROM records ORDER BY DEPT ASC').all().map(a => a['DEPT'])
+        return (await db.query('SELECT DISTINCT DEPT FROM records ORDER BY DEPT ASC')).map(a => a['DEPT'])
     }, log))
     
     fastify.get('/api/catalog/list/numbers', timer(async (request, reply) => {
         reply.type('application/json')
-        return db.prepare('SELECT DISTINCT CATALOG_NBR FROM records ORDER BY DEPT ASC').all().map(a => (typeof a['CATALOG_NBR'] === 'string' ? a['CATALOG_NBR'].trim() : a['CATALOG_NBR']))
+        return (await db.query('SELECT DISTINCT CATALOG_NBR FROM records ORDER BY DEPT ASC')).map(a => (typeof a['CATALOG_NBR'] === 'string' ? a['CATALOG_NBR'].trim() : a['CATALOG_NBR']))
     }, log))
 
     fastify.get('/api/catalog/list/courses/:term/:dept', timer(async (request, reply) => {
         reply.type('application/json')
         if(request.params.term.toLowerCase() === 'all') {
             if(request.params.dept.toLowerCase() === 'all') {
-                return db.prepare('SELECT DISTINCT DEPT, CATALOG_NBR FROM records ORDER BY DEPT ASC, CATALOG_NBR ASC').all().map(a => `${a['DEPT']} ${a['CATALOG_NBR']}`)
+                return (await db.query('SELECT DISTINCT DEPT, CATALOG_NBR FROM records ORDER BY DEPT ASC, CATALOG_NBR ASC')).map(a => `${a['DEPT']} ${a['CATALOG_NBR']}`)
             }
             else {
-                return db.prepare('SELECT DISTINCT CATALOG_NBR FROM records WHERE DEPT=? ORDER BY CATALOG_NBR ASC').all(request.params.dept).map(a => a['CATALOG_NBR'])
+                return (await db.execute('SELECT DISTINCT CATALOG_NBR FROM records WHERE DEPT=? ORDER BY CATALOG_NBR ASC', [request.params.dept])).map(a => a['CATALOG_NBR'])
             }
         }
         else {
-            return db.prepare('SELECT DISTINCT CATALOG_NBR FROM records WHERE TERM_CODE=? AND DEPT=? ORDER BY CATALOG_NBR ASC').all(request.params.term, request.params.dept).map(a => a['CATALOG_NBR'])
+            return (await db.execute('SELECT DISTINCT CATALOG_NBR FROM records WHERE TERM_CODE=? AND DEPT=? ORDER BY CATALOG_NBR ASC', [request.params.term, request.params.dept])).map(a => a['CATALOG_NBR'])
         }
     }, log))
     fastify.get('/api/catalog/list/sections/:term/:dept/:course', timer(async (request, reply) => {
         reply.type('application/json')
         if(request.params.term.toLowerCase() === 'all') {
-            return db.prepare('SELECT DISTINCT CLASS_SECTION FROM records WHERE DEPT=? AND CATALOG_NBR=? ORDER BY CATALOG_NBR ASC').all(request.params.dept, request.params.course).map(a => a['CLASS_SECTION'])
+            return (await db.execute('SELECT DISTINCT CLASS_SECTION FROM records WHERE DEPT=? AND CATALOG_NBR=? ORDER BY CATALOG_NBR ASC', [request.params.dept, request.params.course])).map(a => a['CLASS_SECTION'])
         }
         else {
-            return db.prepare('SELECT DISTINCT CLASS_SECTION FROM records WHERE TERM_CODE=? AND DEPT=? AND CATALOG_NBR=? ORDER BY CATALOG_NBR ASC').all(request.params.term, request.params.dept, request.params.course).map(a => a['CLASS_SECTION'])
+            return (await db.execute('SELECT DISTINCT CLASS_SECTION FROM records WHERE TERM_CODE=? AND DEPT=? AND CATALOG_NBR=? ORDER BY CATALOG_NBR ASC', [request.params.term, request.params.dept, request.params.course])).map(a => a['CLASS_SECTION'])
         }
     }, log))
 
     fastify.get('/api/table/:term/:dept/:course/:section', timer(async (request, reply) => {
         reply.type('application/json')
-        return db.prepare('SELECT * FROM records WHERE TERM_CODE=? AND DEPT=? AND CATALOG_NBR=? AND CLASS_SECTION=? ORDER BY TERM_CODE ASC').all(request.params.term, request.params.dept, request.params.course, request.params.section)
+        return (await db.execute('SELECT * FROM records WHERE TERM_CODE=? AND DEPT=? AND CATALOG_NBR=? AND CLASS_SECTION=? ORDER BY TERM_CODE ASC', [request.params.term, request.params.dept, request.params.course, request.params.section]))
     }, log))
 
     fastify.get('/api/table/:term/:dept/:course', timer(async (request, reply) => {
         reply.type('application/json')
         if(request.params.term.toLowerCase() === "all") {
-            return db.prepare('SELECT * FROM records WHERE DEPT=? AND CATALOG_NBR=? ORDER BY TERM_CODE ASC').all(request.params.dept, request.params.course)
+            return (await db.execute('SELECT * FROM records WHERE DEPT=? AND CATALOG_NBR=? ORDER BY TERM_CODE ASC', [request.params.dept, request.params.course]))
         }
         else {
-            return db.prepare('SELECT * FROM records WHERE TERM_CODE=? AND DEPT=? AND CATALOG_NBR=? ORDER BY TERM_CODE ASC').all(request.params.term, request.params.dept, request.params.course)
+            return (await db.execute('SELECT * FROM records WHERE TERM_CODE=? AND DEPT=? AND CATALOG_NBR=? ORDER BY TERM_CODE ASC', [request.params.term, request.params.dept, request.params.course]))
         }
     }, log))
 }
--- api/server.js
+++ api/server.js
@@ -7,9 +7,6 @@ const fastify = require('fastify')({
 	trustProxy: true
 })
 
-const STATIC_CACHE_AGE = 604800 // 1 week in seconds
-const API_CACHE_AGE = 604800
-
 fastify.register(require('fastify-response-time'))
 fastify.register(require('fastify-graceful-shutdown'))
 fastify.register(require('fastify-rate-limit'), {
@@ -17,24 +14,8 @@ fastify.register(require('fastify-rate-limit'), {
 	timeWindow: '1 minute'
 })
 
-const {promisify} = require('util')
-const client = require('redis').createClient({
-	host: 'redis' // docker defines hostname in /etc/hosts
-});
-const redis = {
-	get: promisify(client.get).bind(client),
-	set: promisify(client.set).bind(client),
-	end: promisify(client.end).bind(client)
-}
-
 // Router file for prefixed endpoints
 fastify.register(require('./route'), { prefix: process.env.BASEURL })
-
-fastify.get('/debug', async (req, reply) => {
-	reply.type('application/json')
-	console.log(process.env)
-	return 'Debug info logged'
-})
 
 fastify.listen(3000, '0.0.0.0', (err, address) => {
 	if (err) throw err
@@ -42,24 +23,4 @@ fastify.listen(3000, '0.0.0.0', (err, address) => {
 		console.log('NODE_ENV set to production')
 	}
 	console.log(`server listening on ${address}`)
-});
-
-(async function(){
-	// redis successful (expire in a day)
-	await redis.set('message', 'hello world', 'EX', 60*60*24)
-	console.log(await redis.get('message'))
-	console.log(await redis.get('fake key'))
-
-	// get the client
-	const mysql = require('mysql2/promise');
-	// create the connection
-	const connection = await mysql.createConnection({
-		host: 'cougar-grades.mariadb',
-		user: 'root',
-		password: process.env.MYSQL_ROOT_PASSWORD,
-		database: 'records'
-	});
-	// query database
-	const [rows, fields] = await connection.execute('SELECT * FROM records');
-	console.log(rows)
 })
