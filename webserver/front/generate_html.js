c98530f37caef24ce1c4f77f6e8221789513c64a
Austin Jackson <themacphage@gmail.com>
2019-05-29 00:02:05 -0500

    implemented mockup into handlebars

--- webserver/front/generate_html.js
+++ webserver/front/generate_html.js
@@ -3,22 +3,22 @@
 const fs = require('fs')
 const path = require('path')
 const Handlebars = require('handlebars')
-require('dotenv').config({path: path.resolve(process.cwd(), '..', '..', '.env')})
+require('dotenv').config({path: path.resolve(__dirname, '..', '..', '.env')})
 
-const partials = fs.readdirSync('./html/partials/')
+const partials = fs.readdirSync(path.resolve(__dirname, './html/partials/')) 
 for(let i = 0; i < partials.length; i++) {
 	if(partials[i].endsWith('.hbs')) {
 		// Use filename before extension as partials name and read partial from filesystem
-		console.log(`partial: ${partials[i]}`)
-		Handlebars.registerPartial(path.basename(partials[i],'.hbs'), fs.readFileSync(`./html/partials/${partials[i]}`, {encoding: 'utf8'}))
+		console.log(`🚲🔨 => partial: ${partials[i]}`)
+		Handlebars.registerPartial(path.basename(partials[i],'.hbs'), fs.readFileSync(path.resolve(__dirname, `./html/partials/${partials[i]}`), {encoding: 'utf8'}))
 	}
 }
 
-const markup = fs.readdirSync('./html/')
+const markup = fs.readdirSync(path.resolve(__dirname, './html/'))
 for(let i = 0; i < markup.length; i++) {
 	if(markup[i].endsWith('.hbs')) {
-		let template = Handlebars.compile(fs.readFileSync(`./html/${markup[i]}`, {encoding: 'utf8'}))
-		console.log(`template: ${markup[i]}`)
-		fs.writeFileSync(`./_site/${path.basename(markup[i],'.hbs')}.html`, template(process.env), {encoding: 'utf8'})
+		let template = Handlebars.compile(fs.readFileSync(path.resolve(__dirname, `./html/${markup[i]}`), {encoding: 'utf8'}))
+		console.log(`🚲🔨 => template: ${markup[i]}`)
+		fs.writeFileSync(path.resolve(__dirname, `./_site/${path.basename(markup[i],'.hbs')}.html`), template(process.env), {encoding: 'utf8'})
 	}
 }
--- webserver/front/preprocess.sh
+++ webserver/front/preprocess.sh
@@ -4,24 +4,8 @@ cd "$(dirname "$0")"
 # generated files
 mkdir -p _site
 
-# mustache generate
-echo "
-const path = require('path')
-require('dotenv').config({path: path.resolve(process.cwd(), '..', '..', '.env')})
-console.log(JSON.stringify(process.env))" | node - > env.json
-
-# enumerate all partials
-declare -a part=(`ls mustache/partials/*.mustache`)
-ARG=""
-for i in "${part[@]}"
-do
-	ARG="$ARG -p $i"
-done
-
-# generate templates
-for f in mustache/*.mustache; do 
-    npx mustache $ARG env.json $f > _site/$(basename $f .mustache).html
-done
+# handlebars
+node generate_html.js
 
 # sass generate
 npx sass sass/:_site/css/
