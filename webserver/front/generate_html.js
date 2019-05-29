9ce175ce9cbfc701a631736da30df10803722e12
Austin Jackson <themacphage@gmail.com>
2019-05-28 23:33:33 -0500

    generate handlebars

--- webserver/front/generate_html.js
+++ webserver/front/generate_html.js
@@ -5,20 +5,20 @@ const path = require('path')
 const Handlebars = require('handlebars')
 require('dotenv').config({path: path.resolve(process.cwd(), '..', '..', '.env')})
 
-const partials = fs.readdirSync('./mustache/partials/')
+const partials = fs.readdirSync('./html/partials/')
 for(let i = 0; i < partials.length; i++) {
-	if(partials[i].endsWith('.mustache')) {
+	if(partials[i].endsWith('.hbs')) {
 		// Use filename before extension as partials name and read partial from filesystem
 		console.log(`partial: ${partials[i]}`)
-		Handlebars.registerPartial(path.basename(partials[i],'.mustache'), fs.readFileSync(`./mustache/partials/${partials[i]}`, {encoding: 'utf8'}))
+		Handlebars.registerPartial(path.basename(partials[i],'.hbs'), fs.readFileSync(`./html/partials/${partials[i]}`, {encoding: 'utf8'}))
 	}
 }
 
-const markup = fs.readdirSync('./mustache/')
+const markup = fs.readdirSync('./html/')
 for(let i = 0; i < markup.length; i++) {
-	if(markup[i].endsWith('.mustache')) {
-		let template = Handlebars.compile(fs.readFileSync(`./mustache/${markup[i]}`, {encoding: 'utf8'}))
+	if(markup[i].endsWith('.hbs')) {
+		let template = Handlebars.compile(fs.readFileSync(`./html/${markup[i]}`, {encoding: 'utf8'}))
 		console.log(`template: ${markup[i]}`)
-		fs.writeFileSync(`./_site/${path.basename(markup[i],'.mustache')}.html`, template(process.env), {encoding: 'utf8'})
+		fs.writeFileSync(`./_site/${path.basename(markup[i],'.hbs')}.html`, template(process.env), {encoding: 'utf8'})
 	}
 }
