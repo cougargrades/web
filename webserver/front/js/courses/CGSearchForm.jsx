a6704cad6eda71889d06cdd2b70750ff0fb4e072
Austin Jackson <themacphage@gmail.com>
2019-05-31 17:13:56 -0500

    form is more reactive to input

--- webserver/front/js/courses/CGSearchForm.jsx
+++ webserver/front/js/courses/CGSearchForm.jsx
@@ -9,10 +9,12 @@ class CGSearchForm extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            selection: ["MATH 2331", "ENGL 1304"]
+            selection: ["MATH 2331", "ENGL 1304"],
+            searchbar: "default"
         };
     }
 
+    // Selection display
     addSelection(value) {
         // Immutable solution
         this.setState({
@@ -22,6 +24,17 @@ class CGSearchForm extends React.Component {
     }
     removeSelection(value) {
         // Immutable solution
+        if(this.state.selection.includes(value) && this.state.selection.length === 1) {
+            // If the removal of value is expected to happen 
+            // AND 
+            // the removal would make the selection empty
+            this.setState({
+                searchbar: 'default'
+            })
+        }
+
+        // TODO: use animejs to animate the removal
+
         this.setState({
             selection: this.state.selection.filter(item => item !== value) 
         })
@@ -41,20 +54,57 @@ class CGSearchForm extends React.Component {
         )
     }
     handleBadgeClick(elem) {
-        this.removeSelection(elem)
+        // Don't remove any badges while the form is in the middle of processing
+        if(this.state.searchbar !== 'loading') {
+            this.removeSelection(elem)
+        }
     }
+
+    // Input box
+    handleSubmit(event) {
+        event.preventDefault()
+        console.log('submitted')
+    }
+    handleKeyUp(event) {
+        //console.log(event.target.value, this.state.selection)
+        let field = event.target;
+        if(field.value.length > 0 || this.state.selection.length === 0) {
+            // If selected courses is empty OR text field has text in it
+            this.setState({
+                searchbar: "default"
+            })
+        }
+        else if(this.state.selection.length > 0 && field.value.length === 0) {
+            // If selected courses is not empty AND text field is empty
+            this.setState({
+                searchbar: "confirm"
+            })
+        }
+    }
+    getButtonCSSClass() {
+        if(this.state.searchbar === "loading") {
+            return "btn-cg loading"
+        }
+        else if(this.state.searchbar === "confirm") {
+            return "btn-cg confirm"
+        }
+        else {
+            return "btn-cg"
+        }
+    }
+
 
 
     render() {
         return (
         <div>
-            <Form autoComplete="off">
+            <Form id="search" autoComplete="off" onSubmit={e => this.handleSubmit(e)}>
                 <Form.Group>
                     <Form.Label>Add course</Form.Label>
                     <InputGroup>
-                        <Form.Control type="text" placeholder="ENGL 1304" />
+                        <Form.Control type="text" placeholder="Example: ENGL 1304" onKeyUp={e => this.handleKeyUp(e)}/>
                         <InputGroup.Append>
-                            <Button type="submit" className="btn-cg" onClick={() => {}}>
+                            <Button type="submit" className={this.getButtonCSSClass()} id="searchbar_btn">
                                 <span className="add-msg">Add to selection</span>
                                 <span className="confirm-msg">Search selection</span>
                                 <i className="material-icons">autorenew</i>
