3e904da50dfdc0b4233c231b7616cb388f3214e7
Austin Jackson <themacphage@gmail.com>
2019-08-07 22:06:36 -0500

    Passing Firestore references with ease

--- app/src/components/home/home.js
+++ app/src/components/home/home.js
@@ -11,30 +11,6 @@ import Util from '../_common/util';
 
 import './home.scss';
 
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-
-// Initialize Cloud Firestore through Firebase
-firebase.initializeApp({
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID
-});
-
-firebase.firestore().enablePersistence().catch(function(err) {
-    if (err.code === 'failed-precondition') {
-        // Multiple tabs open, persistence can only be enabled
-        // in one tab at a a time.
-        // ...
-    } else if (err.code === 'unimplemented') {
-        // The current browser does not support all of the
-        // features required to enable persistence
-        // ...
-    }
-})
-
-var db = firebase.firestore();
-
 class Home extends Component {
     state = {
         latestTerm: '...'
@@ -48,6 +24,7 @@ class Home extends Component {
 
     componentDidMount() {
         let self = this;
+        let db = this.props.db; // Firestore reference passed successfully without creating another instance
         for(let i in this.styles.body){
             document.body.style[i] = this.styles.body[i];
         }
--- app/src/components/root/root.js
+++ app/src/components/root/root.js
@@ -17,38 +17,64 @@ import About from '../about/about';
 import Info from '@material-ui/icons/Info';
 import Lock from '@material-ui/icons/Lock';
 
-class Root extends Component {
-    render() {
-      return (
-      <Router>
-        <Navbar bg="light" expand="lg" className="cg-navbar">
-          <Navbar.Brand>
-            <Brand />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-              <Nav.Link as={Link} to="/">Home</Nav.Link>
-              <Nav.Link as={Link} to="/courses">Courses</Nav.Link>
-              <Nav.Link as={Link} to="/instructors" disabled={true}><Lock/>Instructors</Nav.Link>
-              <Nav.Link as={Link} to="/groups" disabled={true}><Lock/>Groups</Nav.Link>
-              
-            </Nav>
-            <Nav className="justify-content-end">
-              <Nav.Link as={Link} to="/about"><Info/>About</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/courses" component={Courses} />
-          {/* <Route path="/instructors" exact component={Home} /> */}
-          {/* <Route path="/groups" exact component={Home} /> */}
-          <Route path="/about" component={About} />
-        </Switch>
-      </Router>
-      );
+class Root extends Component {
+    constructor() {
+        super()
+        // Initialize Cloud Firestore through Firebase
+        firebase.initializeApp({
+            apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+            authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+            projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID
+        });
+
+        firebase.firestore().enablePersistence().catch(function(err) {
+            if (err.code === 'failed-precondition') {
+                    // Multiple tabs open, persistence can only be enabled
+                    // in one tab at a a time.
+                    // ...
+            } else if (err.code === 'unimplemented') {
+                    // The current browser does not support all of the
+                    // features required to enable persistence
+                    // ...
+            }
+        })
+
+        this.firebase = firebase;
+        this.db = firebase.firestore();
+    }
+    render() {
+        return (
+        <Router>
+            <Navbar bg="light" expand="lg" className="cg-navbar">
+                <Navbar.Brand>
+                    <Brand />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link as={Link} to="/">Home</Nav.Link>
+                        <Nav.Link as={Link} to="/courses">Courses</Nav.Link>
+                        <Nav.Link as={Link} to="/instructors" disabled={true}><Lock/>Instructors</Nav.Link>
+                        <Nav.Link as={Link} to="/groups" disabled={true}><Lock/>Groups</Nav.Link>
+                    </Nav>
+                    <Nav className="justify-content-end">
+                        <Nav.Link as={Link} to="/about"><Info/>About</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+
+            <Switch>
+                <Route path="/" exact component={() => <Home firebase={this.firebase} db={this.db} />} />
+                <Route path="/courses" component={() => <Courses firebase={this.firebase} db={this.db} />} />
+                {/* <Route path="/instructors" exact component={Home} /> */}
+                {/* <Route path="/groups" exact component={Home} /> */}
+                <Route path="/about" component={() => <About firebase={this.firebase} db={this.db} />} />
+            </Switch>
+        </Router>
+        );
     }
 }
 
