6b6477c60637c37b65e44247342e3cc7565de6ae
Austin Jackson <themacphage@gmail.com>
2020-03-21 20:09:39 -0500

    analytics + service worker research

--- src/components/_common/firebase.js
+++ src/components/_common/firebase.js
@@ -3,12 +3,20 @@
 //import * as firebase from 'firebase';
 import firebase from 'firebase/app'
 import 'firebase/firestore'
+import 'firebase/analytics'
+import 'firebase/performance'
 
 const config = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    measurementId: '<redacted>' // as was previously defined in static index.html
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 export default !firebase.apps.length ? firebase.initializeApp(config) : firebase.app();
+firebase.analytics()
+firebase.performance()
--- workbox-config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  "globDirectory": "build/prod/",
-  "globPatterns": [
-    "**/*.{ico,html,js,css}"
-  ],
-  "swDest": "build/prod/sw.js"
-};
