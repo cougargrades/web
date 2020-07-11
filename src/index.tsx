b11050917d1980dd47d3d7f449802fbcb92d4564
Austin Jackson <themacphage@gmail.com>
2020-07-11 16:47:12 -0500

    updated snowpack 2.2.0 -> 2.6.4, npm audit fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 //import Root from './components/root/root';
 const Root = React.lazy(() => import('./components/root/root'));
 
