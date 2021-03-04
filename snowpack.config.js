90818d1507289afce2820080a749d79a558428ae
Austin Jackson <themacphage@gmail.com>
2021-03-04 16:05:26 -0600

    moved to snowpack 3

--- snowpack.config.js
+++ snowpack.config.js
@@ -8,25 +8,40 @@ module.exports = {
     '@snowpack/plugin-react-refresh',
     '@snowpack/plugin-dotenv',
     '@snowpack/plugin-typescript',
-    ['@snowpack/plugin-optimize', { target: 'es2017', preloadModules: true }],
+    //['@snowpack/plugin-optimize', { target: 'es2017', preloadModules: true }],
     '@snowpack/plugin-sass'
   ],
-  install: [
-    /* ... */
+  routes: [
+    /* Enable an SPA Fallback in development: */
+    {"match": "routes", "src": ".*", "dest": "/index.html"},
   ],
-  installOptions: {
+  optimize: {
+    /* Example: Bundle your final build: */
+    // "bundle": true,
+    preload: true,
+    bundle: false,
+    minify: true,
+    target: 'es2020'
+    // {
+    //   entrypoints: 'auto' | string[] | ((options: { files: string[] }) => string[]);
+    //   preload: boolean;
+    //   bundle: boolean;
+    //   splitting: boolean;
+    //   treeshake: boolean;
+    //   manifest: boolean;
+    //   minify: boolean;
+    //   target: 'es2020' | 'es2019' | 'es2018' | 'es2017';
+    // }
+    
+    
+  },
+  packageOptions: {
     /* ... */
   },
   devOptions: {
     /* ... */
   },
   buildOptions: {
-    /* ... */
-  },
-  proxy: {
-    /* ... */
-  },
-  alias: {
     /* ... */
   },
 };
