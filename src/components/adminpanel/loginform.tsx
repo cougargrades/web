2dfbac9d1e837ddcea73d8640a47c3c63af2a2e7
Austin Jackson <themacphage@gmail.com>
2021-03-30 17:18:23 -0500

    added deleteAccount and small disclaimer

--- src/components/adminpanel/loginform.tsx
+++ src/components/adminpanel/loginform.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { auth as fbAuth, useAuth } from 'reactfire';
 import Collaborator from '../collaborator/collaborator';
 
+import './loginform.scss';
+
 export function LoginForm() {
   const auth = useAuth();
   const provider = new fbAuth.GoogleAuthProvider();
@@ -16,6 +18,17 @@ export function LoginForm() {
     auth.signOut();
   }
 
+  const deleteAccount = async () => {
+    if(auth.currentUser) {
+      // this is a sensitive operation so we want to revalidate their identity
+      const cred = await auth.currentUser.reauthenticateWithPopup(provider);
+      // if reauthentication was successful
+      if(cred.credential) {
+        await auth.currentUser.delete();
+      }
+    }
+  };
+
   // If already signed in, offer account options
   if (auth.currentUser) return (
     <div>
@@ -29,8 +42,9 @@ export function LoginForm() {
           avatar_url={auth.currentUser.photoURL!}
         />
       </div>
-      <div>
+      <div className="button-spread">
         <button onClick={signOut}>Sign Out</button>
+        <button onClick={deleteAccount} disabled={auth.currentUser === null}>Delete account</button>
       </div>
     </div>
   );
@@ -40,6 +54,7 @@ export function LoginForm() {
     <div>
       <h4>Authentication required</h4>
       <p>Only authorized users will be permitted to access. Before proceeding, please sign in.</p>
+      <p><small><em>By signing in, certain information about your Google account is <a href="https://github.com/cougargrades/api/blob/766cfd4c840712d59c2594dde19e2ecd4f3814b8/functions/src/firestore/saveUserToDatabase.ts#L16-L18">preserved in our database</a>. This is limited to: your display name, your email, and the URL of your Google account picture. Users will have the option of having their account <a href="https://github.com/cougargrades/api/blob/766cfd4c840712d59c2594dde19e2ecd4f3814b8/functions/src/firestore/deleteUserFromDatabase.ts#L4-L20">deleted from our database</a>. CougarGrades.io is open-source and not for profit. We are not interested in contacting you with this information or sharing it with third-parties.</em></small></p>
       <button onClick={signIn}>Sign in with Google</button>
     </div>
   );
