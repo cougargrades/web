b504f0da4ca040935086c6e54887768bfe0fc62a
Austin Jackson <themacphage@gmail.com>
2021-04-23 18:11:52 -0500

    added easter egg

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory, useLocation } from 'react-router';
 
 import './footer.scss';
 
@@ -7,10 +8,25 @@ interface FooterProps {
 }
 
 export default function Footer(props: FooterProps) {
+  const history = useHistory();
+  const [easterEgg, setEasterEgg] = useState<number>(0);
+
+  const handleClick = (e: React.MouseEvent<HTMLHeadingElement, MouseEvent>) => {
+    e.preventDefault();
+    setEasterEgg(x => x + 1);
+  }
+
+  useEffect(() => {
+    if(easterEgg >= 3) {
+      setEasterEgg(0);
+      history.push('/admin');
+    }
+  }, [easterEgg]);
+
   const commitHash = import.meta.env.SNOWPACK_PUBLIC_GIT_SHA; // hopefully works
   return (
     <footer>
-      <h6>@cougargrades/web</h6>
+      <h6 onClick={handleClick}>@cougargrades/web</h6>
       <p>
         Version: {import.meta.env.SNOWPACK_PUBLIC_VERSION}, Commit:{' '}
         <a href={`https://github.com/cougargrades/web/commit/${commitHash}`}>
