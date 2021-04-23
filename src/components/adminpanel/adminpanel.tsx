f38915b225577305249433ba66ed2c36d0333342
Austin Jackson <themacphage@gmail.com>
2021-04-23 16:38:04 -0500

    better way of detecting custom claims

--- src/components/adminpanel/adminpanel.tsx
+++ src/components/adminpanel/adminpanel.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import { useUser, useIdTokenResult } from 'reactfire/dist/index';
+import { CustomClaimNames as isCustomClaim } from '@cougargrades/types/dist/is'
 
 export default function AdminPanel() {
   // get the current user, identified by the SDK-managed JWT 
   const { data: user } = useUser();
   const { data: jwt, error } = useIdTokenResult(user, true);
-  // dont present other stuff present in the OpenID spec
-  // see: https://openid.net/specs/openid-connect-core-1_0.html#IDToken
-  const custom_claims = ['admin'];
   
   // typical SWR stuff
   if (error) return <div>failed to load</div>;
   if (!jwt) return <div>loading...</div>;
 
   const rows = Object.keys(jwt.claims)
-  .filter(e => custom_claims.includes(e))
+  // dont present other stuff present in the OpenID spec
+  // see: https://openid.net/specs/openid-connect-core-1_0.html#IDToken
+  .filter(e => isCustomClaim(e))
   .map(key => 
     <tr key={key}>
       <td>{key}</td>
