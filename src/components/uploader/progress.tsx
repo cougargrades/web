30988c025b7856383fc8a61689315ba681e7d735
Austin Jackson <themacphage@gmail.com>
2021-04-27 01:15:11 -0500

    improved progress bars

--- src/components/uploader/progress.tsx
+++ src/components/uploader/progress.tsx
@@ -2,30 +2,32 @@ import React from 'react';
 
 import './progress.scss';
 
-/**
- * React wrapper for Bootstrap@5.0.0 progress bar
- * Adapted from: https://getbootstrap.com/docs/5.0/components/progress/
- */
-export function Progress(props: { value: number, max: number, children?: React.ReactNode }) {
-  const { value, max } = props;
-  return (
-    <div className="progress">
-      <div className="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" style={{width: `${value/max * 100}%`}}>
-        {props.children}
-      </div>
-    </div>
-  );
-}
-
 // See: https://getbootstrap.com/docs/5.0/customize/color/
-type ColorVariants = 'bg-primary' | 'bg-secondary' | 'bg-success' | 'bg-danger' | 'bg-warning' | 'bg-info' | 'bg-light' | 'bg-dark' |
+export type ProgressColorVariants = 'bg-primary' | 'bg-secondary' | 'bg-success' | 'bg-danger' | 'bg-warning' | 'bg-info' | 'bg-light' | 'bg-dark' |
 'bg-blue' | 'bg-indigo' | 'bg-purple' | 'bg-pink' | 'bg-red' | 'bg-orange' | 'bg-yellow' | 'bg-green' | 'bg-teal' | 'bg-cyan' | 'bg-gray-500' | 'bg-black' | 'bg-white';
 
 /**
  * React wrapper for Bootstrap@5.0.0 progress bar
  * Adapted from: https://getbootstrap.com/docs/5.0/components/progress/
  */
- export function MultiProgress(props: { bars: { key: React.Key, value: number, variant: ColorVariants }[], max: number }) {
+export function Progress(props: { value: number, max: number, variant: ProgressColorVariants, children?: React.ReactNode }) {
+  const { value, max, variant } = props;
+  return (
+    <div className="progress">
+      <div className={`progress-bar progress-bar-striped progress-bar-animated ${variant}`} role="progressbar" style={{width: `${value/max * 100}%`}}>
+        {props.children}
+      </div>
+    </div>
+  );
+}
+
+export type MultiBar = { key: React.Key, value: number, variant: ProgressColorVariants };
+
+/**
+ * React wrapper for Bootstrap@5.0.0 progress bar
+ * Adapted from: https://getbootstrap.com/docs/5.0/components/progress/
+ */
+ export function MultiProgress(props: { bars: MultiBar[], max: number }) {
   const { bars, max } = props;
   return (
     <div className="progress">
