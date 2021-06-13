ce0620446873a657066c77c744176ab1a40954d1
Austin Jackson <themacphage@gmail.com>
2021-06-13 12:53:36 -0500

    added some basic nextjs stuff

---
+++ components/footer.tsx
@@ -0,0 +1,59 @@
+import { buildArgs } from '../lib/environment'
+import styles from './footer.module.scss'
+
+export default function Footer(props: { hideDisclaimer?: boolean }) {
+  // const history = useHistory();
+  // const [easterEgg, setEasterEgg] = useState<number>(0);
+
+  // const handleClick = (e: React.MouseEvent<HTMLHeadingElement, MouseEvent>) => {
+  //   e.preventDefault();
+  //   setEasterEgg(x => x + 1);
+  // }
+
+  // useEffect(() => {
+  //   if(easterEgg >= 3) {
+  //     setEasterEgg(0);
+  //     history.push('/admin');
+  //   }
+  // }, [easterEgg]);
+
+  const { commitHash, version, buildDate, vercelEnv } = buildArgs; // hopefully works
+  return (
+    <footer className={styles.footer}>
+      <h6 title={`vercel env: ${vercelEnv}`}>@cougargrades/web</h6>
+      <p>
+        Version: {version}, Commit:{' '}
+        <a href={`https://github.com/cougargrades/web/commit/${commitHash}`}>
+          {typeof commitHash === 'string'
+            ? commitHash.substring(0, 7)
+            : commitHash}
+        </a>
+        <br />
+        Build date:{' '}
+        <span
+          title={new Date(
+            buildDate,
+          ).toUTCString()}
+        >
+          {new Date(
+            buildDate,
+          ).toLocaleDateString()}
+        </span>
+        <br />
+        <a href="https://github.com/cougargrades/web/wiki/Feedback">
+          Got feedback?
+        </a>
+      </p>
+      {props.hideDisclaimer ? (
+        <></>
+      ) : (
+        <p>
+          <em>
+            Not affiliated with the University of Houston. Data is sourced
+            directly from the University of Houston.
+          </em>
+        </p>
+      )}
+    </footer>
+  );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
-import Head from 'next/head';
+import Head from 'next/head'
+import { FirebaseAppProvider } from 'reactfire'
+import { firebaseConfig } from '../lib/environment';
+import '@cougargrades/raster/raster.grid.css'
+import 'normalize.css/normalize.css'
+import '@exampledev/new.css/new.css'
 import '../styles/globals.scss'
+import '../styles/colors.scss'
 
-function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
@@ -12,6 +18,7 @@ function MyApp({ Component, pageProps }) {
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" href="/favicon.ico" />
         {/* Verification */}
         <meta name="google-site-verification" content="6Ci3V-jOwFCqFvntbRrkdoxF7MB4DC5gI_wWNz9fNTI" />
         {/* SEO */}
@@ -37,9 +44,9 @@ function MyApp({ Component, pageProps }) {
         <meta name="twitter:creator" content="@author_handle" />
         <meta name="twitter:image:src" content="http://www.example.com/image.jpg" />  */}
       </Head>
-      <Component {...pageProps} />
+      <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+        <Component {...pageProps} />
+      </FirebaseAppProvider>
     </>
   );
 }
-
-export default MyApp
