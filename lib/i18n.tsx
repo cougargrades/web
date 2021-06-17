d363c3c2e43d88f520fdebcb60244a5973b6e598
Austin Jackson <themacphage@gmail.com>
2021-06-17 17:48:51 -0500

    ok maybe this time

--- lib/i18n.tsx
+++ lib/i18n.tsx
@@ -11,7 +11,7 @@ export function useRosetta() {
   // create and maintain an instance of rosetta
   const [stone, _] = useState(rosetta(config));
   // set the default locale, but replace hyphens with underscores because variable names cant contain hyphens in JS
-  if(locale) stone.locale(locale.replaceAll('-','_'));
+  stone.locale(locale.replace('-','_'));
   // return the instance
   return stone;
 }
