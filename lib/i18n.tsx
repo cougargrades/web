613c009accb1a0ad3ef07c8a3e1e82ea8fd7ebd6
Austin Jackson <themacphage@gmail.com>
2021-06-17 17:56:54 -0500

    i18n proof-of-concept complete

--- lib/i18n.tsx
+++ lib/i18n.tsx
@@ -6,8 +6,6 @@ import * as config from '../i18n/config'
 export function useRosetta() {
   // get the locale defined by Next.js
   const locale = useRouter().locale;
-  console.log('locale?', locale);
-  console.log('config?', config);
   // create and maintain an instance of rosetta
   const [stone, _] = useState(rosetta(config));
   // set the default locale, but replace hyphens with underscores because variable names cant contain hyphens in JS
