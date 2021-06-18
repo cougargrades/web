7e7bf792f53621b077cced220a8264e350c9dd67
Austin Jackson <themacphage@gmail.com>
2021-06-18 13:30:37 -0500

    fixed #38 and modified styles

--- components/header.tsx
+++ components/header.tsx
@@ -1,39 +1,8 @@
 import Link from 'next/link'
 import Button from '@material-ui/core/Button'
-//import Icon from '@material-ui/core/Icon'
-import { withStyles } from '@material-ui/core/styles'
-//import { Emoji } from './emoji'
 import styles from './header.module.scss'
-import navbubble from './navbubble.module.scss'
 
-
-export const NavBubble = withStyles(theme => ({
-  root: {
-    textDecoration: 'none' ,
-    textTransform: 'none',
-    padding: '0.25em 0.75em',
-    paddingLeft: 'calc(0.75em + 4px)',
-    borderRadius: '8px',
-    fontWeight: 500,
-    fontSize: 'inherit',
-    lineHeight: 1.5,
-    
-    color: '#0070f3',
-    backgroundColor: theme.palette.type === 'dark' ? '#2b2b2b' : '#e6ebf1',
-    '&:active': {
-      outline: 0,
-    },
-    '&:focus': {
-      outline: 0,
-      backgroundColor: theme.palette.type === 'dark' ? '##323232' : '#dfe6ed', 
-    },
-    '&:hover': {
-      backgroundColor: theme.palette.type === 'dark' ? '#323232' : '#dfe6ed',
-    },
-  },
-}))(Button);
-
-export const NavBubbleLink = ({ href, children }) => <Link href={href} passHref><NavBubble>{children}</NavBubble></Link>
+export const NavLink = ({ href, children }) => <Link href={href} passHref><Button variant="contained" disableElevation>{children}</Button></Link>
 
 export default function Header() {
   return (
@@ -46,12 +15,13 @@ export default function Header() {
             course
           </h3>
         </hgroup>
-        <nav className={navbubble.nav}>
-          <NavBubbleLink href="/">🏠 Home</NavBubbleLink>
-          <NavBubbleLink href="/courses">📚 Courses</NavBubbleLink>
-          <NavBubbleLink href="/instructors">👩‍🏫 Instructors</NavBubbleLink>
-          <NavBubbleLink href="/groups">🗃️ Groups</NavBubbleLink>
-          <NavBubbleLink href="/about">👋 About</NavBubbleLink>
+        <nav className={styles.nav}>
+          <NavLink href="/">🏠 Home</NavLink>
+          <NavLink href="/courses">📚 Courses</NavLink>
+          <NavLink href="/instructors">👩‍🏫 Instructors</NavLink>
+          <NavLink href="/groups">🗃️ Groups</NavLink>
+          <NavLink href="/about">👋 About</NavLink>
+          <Button variant="contained" disabled disableElevation>🔥 Disabled</Button>
         </nav>
       </div>
     </header>
