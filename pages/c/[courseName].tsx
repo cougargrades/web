66a42c0185c1697140e99490faadfea91e6dd23f
Austin Jackson <themacphage@gmail.com>
2021-07-18 21:49:51 -0500

    try #43 again

--- pages/c/[courseName].tsx
+++ pages/c/[courseName].tsx
@@ -3,13 +3,13 @@ import Head from 'next/head'
 import { useRouter } from 'next/router'
 import Container from '@material-ui/core/Container'
 import { PankoRow } from '../../components/panko'
-import { getQueryValue } from '../../lib/query'
 
 export default function IndividualCourse() {
   // router.query is inaccessible on the first render, bad for SEO
   // See: https://github.com/vercel/next.js/discussions/11484#discussioncomment-60563
-  const router = useRouter()
-  const courseName = getQueryValue(router, 'courseName')
+  const { query, isReady } = useRouter()
+  const { courseName } = query
+  if(!isReady) return <></>;
 
   return (
     <>
