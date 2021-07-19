4149b80cedd71cbf024f7df68c64d72d99f450cc
Austin Jackson <themacphage@gmail.com>
2021-07-19 13:54:39 -0500

    improved search using exact title matches

--- lib/data/useSearchResults.ts
+++ lib/data/useSearchResults.ts
@@ -67,22 +67,60 @@ function getFirst<T>(arr: (T | undefined)[]): T | undefined {
   return undefined
 }
 
+// reference: https://stackoverflow.com/a/1129270/4852536
+const sortByTitle = (inputValue: string) => (a: SearchResult, b: SearchResult) => {
+  // if A matches title but B doesn't
+  if(a.title.toUpperCase().startsWith(inputValue.toUpperCase()) && !b.title.toUpperCase().startsWith(inputValue.toUpperCase())) {
+    return -2;
+  }
+  // if B matches title but A doesn't
+  else if(! a.title.toUpperCase().startsWith(inputValue.toUpperCase()) && b.title.toUpperCase().startsWith(inputValue.toUpperCase())) {
+    return 2;
+  }
+  else {
+    // otherwise, return alphabetically
+    return a.title.toUpperCase() < b.title.toUpperCase() ? -1 : (a.title.toUpperCase() > b.title.toUpperCase() ? 1 : 0);
+  }
+};
+
 export function useSearchResults(inputValue: string): Observable<SearchResult[]> {
-  const SEARCH_RESULT_LIMIT = 3;
+  const SEARCH_RESULT_LIMIT = 5;
+  const COURSE_EXACT_SEARCH_RESULT_LIMIT = 3;
+  const COURSE_SEARCH_RESULT_LIMIT = 2;
   const db = useFirestore()
-  const courseQuery = db.collection('catalog').where('keywords', 'array-contains', inputValue.toLowerCase()).orderBy('_id').limit(SEARCH_RESULT_LIMIT)
+  // Search for courses
+  const courseQuery = db.collection('catalog').where('keywords', 'array-contains', inputValue.toLowerCase()).limit(COURSE_SEARCH_RESULT_LIMIT)
   const courseData = useFirestoreCollectionData<Course>(courseQuery)
+  // Search for courses that start with the given department code
+  // reference: https://stackoverflow.com/a/57290806/4852536
+  const courseByDeptQuery = db.collection('catalog').where('department', '>=', inputValue.toUpperCase()).where('department', '<', inputValue.toUpperCase().replace(/.$/, c => String.fromCharCode(c.charCodeAt(0) + 1))).limit(COURSE_EXACT_SEARCH_RESULT_LIMIT)
+  const courseByDeptData = useFirestoreCollectionData<Course>(courseByDeptQuery);
+  // Search for instructors
   const instructorQuery = db.collection('instructors').where('keywords', 'array-contains', inputValue.toLowerCase()).orderBy('lastName').limit(SEARCH_RESULT_LIMIT)
   const instructorData = useFirestoreCollectionData<Instructor>(instructorQuery)
+  // Search for groups
   const groupQuery = db.collection('groups').where('keywords', 'array-contains', inputValue.toLowerCase()).orderBy('name').limit(SEARCH_RESULT_LIMIT)
   const groupData = useFirestoreCollectionData<Group>(groupQuery)
 
   return {
     data: [
-      ...(courseData.status === 'success' ? courseData.data.map(e => course2Result(e)) : []),
-      ...(instructorData.status === 'success' ? instructorData.data.map(e => instructor2Result(e)) : []),
-      ...(groupData.status === 'success' ? groupData.data.map(e => group2Result(e)) : []),
+      ...[
+        ...(courseByDeptData.status === 'success' ? courseByDeptData.data.map(e => course2Result(e)) : []),
+        ...(courseData.status === 'success' ? courseData.data.map(e => course2Result(e)) : [])
+      ]
+        // remove duplicates
+        // reference: https://stackoverflow.com/a/56757215/4852536
+        .filter((item, index, self) => index === self.findIndex(e => (e.key === item.key)))
+        // put exact title matches first
+        .sort(sortByTitle(inputValue)),
+      ...(instructorData.status === 'success' ? instructorData.data.map(e => instructor2Result(e)) : []).sort(sortByTitle(inputValue)),
+      ...(groupData.status === 'success' ? groupData.data.map(e => group2Result(e)) : []).sort(sortByTitle(inputValue)),
     ],
+    /**
+     * If compareFunction(a, b) returns value > than 0, sort b before a.
+     * If compareFunction(a, b) returns value ≤ 0, leave a and b in the same order.
+     * If inconsistent results are returned, then the sort order is undefined.
+     */
     error: getFirst([courseData.error, instructorData.error, groupData.error]),
     status: inputValue === '' ? 'success' : [courseData.status, instructorData.status, groupData.status].some(e => e === 'loading') ? 'loading' : 'success'
   }
