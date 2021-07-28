5e35325a6c0058d49d1a9dde95f6170087836788
Austin Jackson <themacphage@gmail.com>
2021-07-28 02:43:47 -0500

    added source

--- pages/groups.tsx
+++ pages/groups.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Container from '@material-ui/core/Container'
+import Chip from '@material-ui/core/Chip'
 import { PankoRow } from '../components/panko'
 import { useAllGroups } from '../lib/data/useAllGroups'
 import { GroupRow } from '../components/grouprow'
@@ -18,6 +19,8 @@ export default function Groups() {
         <PankoRow />
         <main>
           <h1>UH Core Curriculum</h1>
+          <h6>Source:</h6>
+          <Chip label="UH Core Curriculum 2020-2021 " component="a" href="http://publications.uh.edu/content.php?catoid=36&navoid=13119" clickable />
           { status === 'success' ? data.core_curriculum.map(e => (
             <GroupRow key={e.key} data={e} />
           )) : '' }
