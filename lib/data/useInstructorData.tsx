517fda6a03bd0569483a1425ae892a88ab1d218f
Austin Jackson <themacphage@gmail.com>
2021-09-06 17:57:26 -0500

    fixed #58

--- lib/data/useInstructorData.tsx
+++ lib/data/useInstructorData.tsx
@@ -129,6 +129,15 @@ export function useInstructorData(instructorName: string): Observable<Instructor
               valueFormatter: value => `${stone.t(`season.${seasonCode(value)}`)} ${getYear(value)}`,
             },
             {
+              field: 'courseName',
+              headerName: 'Course',
+              type: 'string',
+              width: 65,
+              padding: 8,
+              // eslint-disable-next-line react/display-name
+              valueFormatter: value => <Link href={`/c/${encodeURI(value)}`}><a>{value}</a></Link>,
+            },
+            {
               field: 'sectionNumber',
               headerName: 'Section #',
               type: 'number',
