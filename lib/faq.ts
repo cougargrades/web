0c910d69ee3c63d59ca3c8b4639fec764df9cbfe
Austin Jackson <themacphage@gmail.com>
2022-05-04 19:55:48 -0500

    stray comment

--- lib/faq.ts
+++ lib/faq.ts
@@ -14,16 +14,6 @@ import rehypeStringify from 'rehype-stringify'
 
 const postsDirectory = join(process.cwd(), '_posts')
 
-// title: 'Learn How to Pre-render Pages Using Static Generation with Next.js'
-// excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent elementum facilisis leo vel fringilla est ullamcorper eget. At imperdiet dui accumsan sit amet nulla facilities morbi tempus.'
-// coverImage: '/assets/blog/hello-world/cover.jpg'
-// date: '2020-03-16T05:35:07.322Z'
-// author:
-//   name: Tim Neutkens
-//   picture: '/assets/blog/authors/tim.jpeg'
-// ogImage:
-//   url: '/assets/blog/hello-world/cover.jpg'
-
 export interface FaqPostData {
   slug?: string;
   title?: string;
