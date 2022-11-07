bcde25b981a12ff65f698e29d5b3aacd76579abc
Austin Jackson <themacphage@gmail.com>
2022-11-07 16:16:47 -0600

    cache header test

--- pages/api/trending.ts
+++ pages/api/trending.ts
@@ -4,5 +4,7 @@ import { getTrending } from '../../lib/trending';
 
 export default async function Trending(req: NextApiRequest, res: NextApiResponse) {
   const data = await getTrending();
+  const cacheDuration = 259200; // 3 days in seconds
+  res.setHeader('Cache-Control', `s-maxage=1, stale-while-revalidate=${cacheDuration}`);
   res.json(data);
 }
