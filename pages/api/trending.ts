f3dd2082ab9544c9fd3da756b237f2b7f1f767f8
Austin Jackson <themacphage@gmail.com>
2022-11-07 16:21:27 -0600

    i think this is fine

--- pages/api/trending.ts
+++ pages/api/trending.ts
@@ -5,7 +5,6 @@ import { getTrending } from '../../lib/trending';
 export default async function Trending(req: NextApiRequest, res: NextApiResponse) {
   const data = await getTrending();
   const maxAge = 86400; // 1 day in seconds
-  const swrDuration = 259200; // 3 days in seconds
-  res.setHeader('Cache-Control', `s-maxage=${maxAge}, stale-while-revalidate=${swrDuration}`);
+  res.setHeader('Cache-Control', `s-maxage=${maxAge}, stale-while-revalidate=${maxAge}`);
   res.json(data);
 }
