837aa075727a1b70c262f366d61d1d1833c19481
Austin Jackson <themacphage@gmail.com>
2023-01-22 18:10:21 -0600

    fixed compile error

--- lib/data/useInstructorData.tsx
+++ lib/data/useInstructorData.tsx
@@ -163,6 +163,7 @@ export function useInstructorData(instructorName: string): Observable<Instructor
           rows: [
             ...(didLoadCorrectly ? sectionData.sort((a,b) => b.term - a.term).map(e => ({
               id: e._id,
+              primaryInstructorName: Array.isArray(e.instructorNames) ? `${e.instructorNames[0].lastName}, ${e.instructorNames[0].firstName}` : '',
               ...e,
             })) : [])
           ],
