ad6b66c0dbd83c7d0a6c955fb419cb043f87f765
Austin Jackson <themacphage@gmail.com>
2023-01-22 21:33:58 -0600

    moved search to new system

--- lib/data/useSearchResults.ts
+++ lib/data/useSearchResults.ts
@@ -1,9 +1,10 @@
 import { useFirestore, useFirestoreCollectionData } from 'reactfire'
 import { Course, Instructor, Group } from '@cougargrades/types'
-import useSWR from 'swr'
+import useSWR from 'swr/immutable'
 import { getGradeForGPA, getGradeForStdDev, grade2Color } from '../../components/badge'
 import { Observable } from './Observable'
 import { getBadges } from './getBadges';
+import { firebaseApp } from '../ssg'
 
 export interface SearchResultBadge {
   key: string;
@@ -81,51 +82,102 @@ const sortByTitle = (inputValue: string) => (a: SearchResult, b: SearchResult) =
   }
 };
 
+export async function getSearchResults(inputValue: string): Promise<SearchResult[]> {
+  const SEARCH_RESULT_LIMIT = 5;
+  const COURSE_EXACT_SEARCH_RESULT_LIMIT = 3;
+  const COURSE_SEARCH_RESULT_LIMIT = 2;
+
+  const db = firebaseApp.firestore();
+  // Search for courses
+  const courseQuery = db.collection('catalog').where('keywords', 'array-contains', inputValue.toLowerCase()).limit(COURSE_SEARCH_RESULT_LIMIT)
+  //const courseData = useFirestoreCollectionData<Course>(courseQuery)
+  // Search for courses that start with the given department code
+  // reference: https://stackoverflow.com/a/57290806/4852536
+  const courseByDeptQuery = db.collection('catalog').where('department', '>=', inputValue.toUpperCase()).where('department', '<', inputValue.toUpperCase().replace(/.$/, c => String.fromCharCode(c.charCodeAt(0) + 1))).limit(COURSE_EXACT_SEARCH_RESULT_LIMIT)
+  //const courseByDeptData = useFirestoreCollectionData<Course>(courseByDeptQuery);
+  // Search for instructors
+  const instructorQuery = db.collection('instructors').where('keywords', 'array-contains', inputValue.toLowerCase()).orderBy('lastName').limit(SEARCH_RESULT_LIMIT)
+  //const instructorData = useFirestoreCollectionData<Instructor>(instructorQuery)
+  // Search for groups
+  const groupQuery = db.collection('groups').where('keywords', 'array-contains', inputValue.toLowerCase()).orderBy('name').limit(SEARCH_RESULT_LIMIT)
+  //const groupData = useFirestoreCollectionData<Group>(groupQuery)
+
+  const searchSnapshots = await Promise.allSettled([
+    courseQuery.get(),
+    courseByDeptQuery.get(),
+    instructorQuery.get(),
+    groupQuery.get(),
+  ])
+
+  const [courseSnap, courseByDeptSnap, instructorSnap, groupSnap] = searchSnapshots;
+
+  return [
+    ...[
+      ...(courseByDeptSnap.status === 'fulfilled' ? courseByDeptSnap.value.docs.map(e => e.data() as Course).map(e => course2Result(e)) : []),
+      ...(courseSnap.status === 'fulfilled' ? courseSnap.value.docs.map(e => e.data() as Course).map(e => course2Result(e)) : []),
+    ]
+      // remove duplicates
+      // reference: https://stackoverflow.com/a/56757215/4852536
+      .filter((item, index, self) => index === self.findIndex(e => (e.key === item.key)))
+      // put exact title matches first
+      .sort(sortByTitle(inputValue)),
+    
+    ...(instructorSnap.status === 'fulfilled' ? instructorSnap.value.docs.map(e => e.data() as Instructor).map(e => instructor2Result(e)).sort(sortByTitle(inputValue)) : []),
+    //...(instructorData.status === 'success' ? instructorData.data.map(e => instructor2Result(e)) : []).sort(sortByTitle(inputValue)),
+    ...(groupSnap.status === 'fulfilled' ? groupSnap.value.docs.map(e => e.data() as Group).map(e => group2Result(e)).sort(sortByTitle(inputValue)) : []),
+    //...(groupData.status === 'success' ? groupData.data.map(e => group2Result(e)) : []).sort(sortByTitle(inputValue)),
+  ]
+}
+
 export function useSearchResults(inputValue: string): Observable<SearchResult[]> {
   const SEARCH_RESULT_LIMIT = 5;
   const COURSE_EXACT_SEARCH_RESULT_LIMIT = 3;
   const COURSE_SEARCH_RESULT_LIMIT = 2;
-  const db = useFirestore()
-  // Search for courses
-  const courseQuery = db.collection('catalog').where('keywords', 'array-contains', inputValue.toLowerCase()).limit(COURSE_SEARCH_RESULT_LIMIT)
-  const courseData = useFirestoreCollectionData<Course>(courseQuery)
-  // Search for courses that start with the given department code
-  // reference: https://stackoverflow.com/a/57290806/4852536
-  const courseByDeptQuery = db.collection('catalog').where('department', '>=', inputValue.toUpperCase()).where('department', '<', inputValue.toUpperCase().replace(/.$/, c => String.fromCharCode(c.charCodeAt(0) + 1))).limit(COURSE_EXACT_SEARCH_RESULT_LIMIT)
-  const courseByDeptData = useFirestoreCollectionData<Course>(courseByDeptQuery);
-  // Search for instructors
-  const instructorQuery = db.collection('instructors').where('keywords', 'array-contains', inputValue.toLowerCase()).orderBy('lastName').limit(SEARCH_RESULT_LIMIT)
-  const instructorData = useFirestoreCollectionData<Instructor>(instructorQuery)
-  // Search for groups
-  const groupQuery = db.collection('groups').where('keywords', 'array-contains', inputValue.toLowerCase()).orderBy('name').limit(SEARCH_RESULT_LIMIT)
-  const groupData = useFirestoreCollectionData<Group>(groupQuery)
+  // const db = useFirestore()
+  // // Search for courses
+  // const courseQuery = db.collection('catalog').where('keywords', 'array-contains', inputValue.toLowerCase()).limit(COURSE_SEARCH_RESULT_LIMIT)
+  // const courseData = useFirestoreCollectionData<Course>(courseQuery)
+  // // Search for courses that start with the given department code
+  // // reference: https://stackoverflow.com/a/57290806/4852536
+  // const courseByDeptQuery = db.collection('catalog').where('department', '>=', inputValue.toUpperCase()).where('department', '<', inputValue.toUpperCase().replace(/.$/, c => String.fromCharCode(c.charCodeAt(0) + 1))).limit(COURSE_EXACT_SEARCH_RESULT_LIMIT)
+  // const courseByDeptData = useFirestoreCollectionData<Course>(courseByDeptQuery);
+  // // Search for instructors
+  // const instructorQuery = db.collection('instructors').where('keywords', 'array-contains', inputValue.toLowerCase()).orderBy('lastName').limit(SEARCH_RESULT_LIMIT)
+  // const instructorData = useFirestoreCollectionData<Instructor>(instructorQuery)
+  // // Search for groups
+  // const groupQuery = db.collection('groups').where('keywords', 'array-contains', inputValue.toLowerCase()).orderBy('name').limit(SEARCH_RESULT_LIMIT)
+  // const groupData = useFirestoreCollectionData<Group>(groupQuery)
   // Get "Trending" data
-  const { data: trendingData, error, isValidating } = useSWR<SearchResult[]>('/api/trending');
+
+  const { data: searchData, error: searchError } = useSWR<SearchResult[]>(`/api/search?${new URLSearchParams({ q: inputValue.toLowerCase() })}`);
+  const { data: trendingData, error: trendingError } = useSWR<SearchResult[]>('/api/trending');
 
   try {
     return {
       data: [
-        ...(!isValidating && Array.isArray(trendingData) ? trendingData : [])
+        ...(Array.isArray(trendingData) ? trendingData : [])
           .filter(trend => trend.title.includes(inputValue)),
-        ...[
-          ...(courseByDeptData.status === 'success' ? courseByDeptData.data.map(e => course2Result(e)) : []),
-          ...(courseData.status === 'success' ? courseData.data.map(e => course2Result(e)) : [])
-        ]
-          // remove duplicates
-          // reference: https://stackoverflow.com/a/56757215/4852536
-          .filter((item, index, self) => index === self.findIndex(e => (e.key === item.key)))
-          // put exact title matches first
-          .sort(sortByTitle(inputValue)),
-        ...(instructorData.status === 'success' ? instructorData.data.map(e => instructor2Result(e)) : []).sort(sortByTitle(inputValue)),
-        ...(groupData.status === 'success' ? groupData.data.map(e => group2Result(e)) : []).sort(sortByTitle(inputValue)),
+        ...(Array.isArray(searchData) ? searchData : []),
+        // ...[
+        //   ...(courseByDeptData.status === 'success' ? courseByDeptData.data.map(e => course2Result(e)) : []),
+        //   ...(courseData.status === 'success' ? courseData.data.map(e => course2Result(e)) : [])
+        // ]
+        //   // remove duplicates
+        //   // reference: https://stackoverflow.com/a/56757215/4852536
+        //   .filter((item, index, self) => index === self.findIndex(e => (e.key === item.key)))
+        //   // put exact title matches first
+        //   .sort(sortByTitle(inputValue)),
+        // ...(instructorData.status === 'success' ? instructorData.data.map(e => instructor2Result(e)) : []).sort(sortByTitle(inputValue)),
+        // ...(groupData.status === 'success' ? groupData.data.map(e => group2Result(e)) : []).sort(sortByTitle(inputValue)),
       ],
       /**
        * If compareFunction(a, b) returns value > than 0, sort b before a.
        * If compareFunction(a, b) returns value ≤ 0, leave a and b in the same order.
        * If inconsistent results are returned, then the sort order is undefined.
        */
-      error: getFirst([courseData.error, instructorData.error, groupData.error]),
-      status: inputValue === '' ? 'success' : [courseData.status, instructorData.status, groupData.status].some(e => e === 'loading') ? 'loading' : 'success'
+      //error: getFirst([courseData.error, instructorData.error, groupData.error]),
+      error: getFirst([ trendingError, searchError ]),
+      status: inputValue === '' ? 'success' : [trendingData, searchData].some(e => !Array.isArray(e)) ? 'loading' : 'success'
     }
   }
   catch(error) {
--- pages/api/search.ts
+++ pages/api/search.ts
@@ -1,11 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { SearchResult } from '../../lib/data/useSearchResults';
+import { getSearchResults, SearchResult } from '../../lib/data/useSearchResults';
 import { days_to_seconds } from '../../lib/to_seconds';
-import { getTrendingResults } from '../../lib/trending';
+import { extract } from '../../lib/util'
 
-export default async function Trending(req: NextApiRequest, res: NextApiResponse<SearchResult[]>) {
-  const data = await getTrendingResults();
+export default async function Search(req: NextApiRequest, res: NextApiResponse<SearchResult[]>) {
+  const { q } = req.query;
+  const data = q !== undefined ? await getSearchResults(extract(q)) : [];
   const maxAge = days_to_seconds(1); // 1 day in seconds
   res.setHeader('Cache-Control', `public, must-revalidate, max-age=${maxAge}`);
   res.json(data);
