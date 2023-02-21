9c2d95c0e963c49fc1b6a976ef3aa9b1e272cc2b
Austin Jackson <themacphage@gmail.com>
2023-02-20 22:47:39 -0600

    i think i'm supposed to switch to this now

--- lib/data/back/getAllGroups.ts
+++ lib/data/back/getAllGroups.ts
@@ -5,7 +5,7 @@ import { AllGroupsResult, AllGroupsResultItem, ALL_GROUPS_SENTINEL, group2Result
 
 export async function getAllGroups(): Promise<AllGroupsResult> {
   const db = firebase.firestore()
-  const query = db.collection('groups').where('categories', 'array-contains', '#UHCoreCurriculum') // TODO: switch to #ShowInSidebar
+  const query = db.collection('groups').where('categories', 'array-contains', '#ShowInSidebar')
   const querySnap = await query.get()
   const data: Group[] = querySnap.docs.filter(e => e.exists).map(e => e.data() as Group);
   
@@ -13,6 +13,7 @@ export async function getAllGroups(): Promise<AllGroupsResult> {
   for(let i = 0; i < data.length; i++) {
     data[i].courses = []
     data[i].sections = []
+    data[i].relatedGroups = []
   }
 
   // make categories, necessary for sorting "correctly"
@@ -20,8 +21,6 @@ export async function getAllGroups(): Promise<AllGroupsResult> {
     ...(
       Array.from(new Set(data.map(e => Array.isArray(e.categories) ? e.categories.filter(cat => !cat.startsWith('#')) : []).flat()))
         .sort((a,b) => defaultComparator(a,b)) // [ '(All)', '(2022-2023)', '(2021-2022)', '(2020-2021)' ]
-        // TODO: TEMPORARY FIX until we switch to '#ShowInSidebar'
-        .slice(0,1) // don't endlessly list the groups, they're still accessible from a course directly
       ),
     //ALL_GROUPS_SENTINEL
   ];
@@ -51,7 +50,7 @@ export async function getAllGroups(): Promise<AllGroupsResult> {
       ...results
     ],
     all_groups: [
-      ...(data.filter(e => Array.isArray(e.categories) && ! e.categories.includes('#UHCoreCurriculum')).map(e => group2Result(e)))
+      ...(data.filter(e => Array.isArray(e.categories) && ! e.categories.includes('#ShowInSidebar')).map(e => group2Result(e)))
     ],
   }
 }
