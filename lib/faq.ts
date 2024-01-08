cefb6031d9f9e88b9d25b2d65138df5df0cee10e
Austin Jackson <themacphage@gmail.com>
2024-01-08 16:05:55 -0600

    fix styling issue

--- lib/faq.ts
+++ lib/faq.ts
@@ -13,6 +13,79 @@ import rehypeSanitize, { defaultSchema } from 'rehype-sanitize'
 import rehypeStringify from 'rehype-stringify'
 import rehypeHighlight from 'rehype-highlight'
 
+// Not direct dependencies, use with caution
+import type { Plugin, Transformer } from 'unified'
+import type { Root, Element } from 'hast'
+import { common } from 'lowlight'
+import HighlightJs from 'highlight.js/lib/core'
+import {visit} from 'unist-util-visit'
+
+/* Turn `js` into `JavaScript`, etc */
+function getFormattedLanguageName(name: string): string | undefined {
+  return common[name]?.(HighlightJs.newInstance())['name'];
+}
+
+/**
+ * On code blocks like ```js, put the language name in a "rel" attribute on the <pre>
+ * 
+ * Reference:
+ * - https://github.com/rehypejs/rehype-highlight/blob/8fbc0ebd3a0c488e7f024ef5af6983c4b49686d1/lib/index.js
+ * - https://github.com/highlightjs/highlight.js/blob/6a52185d9b855130b5acaccef143a7bd602e7885/src/languages/cpp.js#L554
+ */
+export const annotateProgrammingLanguageName: Plugin<any[], Root, Root> = () => async (root, file) => {
+  /* Determines the language used based on the CSS class */
+  const getLanguageFromCssClass = (node: Element): string | undefined => {
+    const classes = node.properties?.className;
+    if (!Array.isArray(classes)) return;
+
+    let name: string | undefined;
+
+    for(let clazz of classes) {
+      const value = String(clazz)
+
+      if (value === 'no-highlight' || value === 'nohighlight') {
+        return undefined;
+      }
+
+      if (!name && value.slice(0, 5) === 'lang-') {
+        name = value.slice(5)
+      }
+
+      if (!name && value.slice(0, 9) === 'language-') {
+        name = value.slice(9)
+      }
+    }
+
+    return name;
+  };
+
+  /* Recursively visits all the nodes by some criteria, much cleaner. */
+  visit(root, 'element', (node, _, parent) => {
+    // Must be a specific arrangement of <pre> > <code> nodes
+    if (
+      node.tagName !== 'code' ||
+      !parent ||
+      parent.type !== 'element' ||
+      parent.tagName !== 'pre'
+    ) {
+      return;
+    }
+
+    // Must be able to determine the language
+    const lang = getLanguageFromCssClass(node);
+    if (!lang) return;
+
+    // Get the formatted name (fallback to codename) and write the "rel" attribute
+    const formattedLang = getFormattedLanguageName(lang) ?? lang;
+    parent.properties = {
+      ...(parent.properties ?? {}),
+      'rel': formattedLang,
+    };
+  });
+  
+  return;
+}
+
 const postsDirectory = join(process.cwd(), '_posts')
 
 export interface FaqPostData {
@@ -67,6 +140,7 @@ export async function markdownToHtml(markdown: string) {
   const result = await remark()
     .use(remarkRehype, { allowDangerousHtml: true })
     .use(rehypeRaw)
+    .use(annotateProgrammingLanguageName)
     .use(rehypeHighlight)
     .use(rehypeSanitize, {
       ...defaultSchema,
@@ -84,6 +158,10 @@ export async function markdownToHtml(markdown: string) {
           ...(defaultSchema.attributes?.['code'] ?? []),
           // List of all allowed languages:
           ['className', 'hljs', 'language-*', 'language-js', 'language-css', 'language-md', 'language-python']
+        ],
+        pre: [
+          ...(defaultSchema.attributes?.['pre'] ?? []),
+          'rel',
         ],
         span: [
           ...(defaultSchema.attributes?.['code'] ?? []),
