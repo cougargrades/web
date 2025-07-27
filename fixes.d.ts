a61f4fde6a7e4ba4a2c1f11cde55ffd3fc625884
Austin Jackson <austinjckson@gmail.com>
2025-07-26 20:02:56 -0500

    removed Cloudflare Workers stuff :(

--- fixes.d.ts
+++ fixes.d.ts
@@ -1,18 +1,4 @@
 
-
-declare module "process" {
-  global {
-    var process: NodeJS.Process;
-    namespace NodeJS {
-      /**
-       * Augments `process.env.*` with those defined in the generated `CloudflareEnv`
-       */
-      interface ProcessEnv extends CloudflareEnv {}
-    }
-  }
-}
-
-
 /**
  * Type error: Could not find a declaration file for module 'react-fitty'. '/Users/austin/sandbox/cougargrades/web/node_modules/react-fitty/dist/react-fitty.esm.js' implicitly has an 'any' type.
  * There are types at '/Users/austin/sandbox/cougargrades/web/node_modules/react-fitty/dist/index.d.ts', but this result could not be resolved when respecting package.json "exports". The 'react-fitty' library may need to update its package.json or typings.
