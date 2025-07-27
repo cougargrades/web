e983afffd1eed621c851066d16a8fb02ec443553
Austin Jackson <austinjckson@gmail.com>
2025-07-26 20:06:48 -0500

    also forgot this fuckin thing

--- next.config.ts
+++ next.config.ts
@@ -32,6 +32,3 @@ const nextConfig: NextConfig = {
 
 export default nextConfig;
 
-// added by create cloudflare to enable calling `getCloudflareContext()` in `next dev`
-import { initOpenNextCloudflareForDev } from '@opennextjs/cloudflare';
-initOpenNextCloudflareForDev();
