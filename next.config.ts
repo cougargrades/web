b8bc78e9ba0500051bfd7beab29d5e0fa2e1746f
Austin Jackson <austinjckson@gmail.com>
2025-07-26 22:06:31 -0500

    changes

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,9 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['avatars.githubusercontent.com', 'lh3.googleusercontent.com'],
   },
+  /**
+   * See: https://nextjs.org/docs/app/api-reference/config/next-config-js/headers
+   */
   headers: async function() {
     return [
       {
@@ -53,7 +56,31 @@ const nextConfig: NextConfig = {
             value: CACHE_CONTROL
           }
         ]
-      }
+      },
+      /**
+       * We also want to cache the `/_next/data/{hash}/{locale}/c/*.json` files that Next.js uses to hydrate the front-end.
+       * Next.js claims that items with this URL are "truly immutable" and as a result we can't override ANY headers for these assets.
+       * 
+       * https://nextjs.org/docs/app/api-reference/config/next-config-js/headers#cache-control
+       * 
+       * Why would we need to? Next.js running locally gives them the Cache-Control of "public, max-age=31536000, immutable", but
+       * Vercel overrides this to "public, max-age=0, must-revalidate" because they want to cache it server-side.
+       * 
+       * The problem is Vercel CHARGES FOR CACHED REQUESTS, so we can't cache "truly immutable" requests in a proxy or browser. 🙄
+       * 
+       * As a workaround, the following Cache Rules are put into place on the Cloudflare side:
+       * 
+       * - Cache `_next/data/{hash}/{locale}/c/*.json` for 7 days
+       *  - Uri Path -> wildcard -> /_next/data/ * / * / c / *.json
+       *  - Edge TTL -> 7 days
+       * - Cache `_next/data/{hash}/{locale}/i/*.json` for 7 days
+       *  - Uri Path -> wildcard -> /_next/data/ * / * / i / *.json
+       *  - Edge TTL -> 7 days
+       * - Cache `_next/data/{hash}/{locale}/g/*.json` for 7 days
+       *  - Uri Path -> wildcard -> /_next/data/ * / * / g / *.json
+       *  - Edge TTL -> 7 days
+       * 
+       */
     ]
   },
   i18n: {
