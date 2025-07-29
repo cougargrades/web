491de58854f257a91501d8b243930a2dc64f4f8a
Austin Jackson <austinjckson@gmail.com>
2025-07-29 11:59:00 -0500

    updated cache lifetime for 'recency' dependent endpoints

--- components/LiveDataDisclaimer.tsx
+++ components/LiveDataDisclaimer.tsx
@@ -7,7 +7,7 @@ import { Temporal } from 'temporal-polyfill'
 export function LiveDataBadge() {
   return (
     <Tooltip title="The data below comes directly from a 3rd party service. It is not stored by CougarGrades and can be removed or changed at any time." arrow>
-      <Chip label="Remote Data" color="primary" size="small" />
+      <Chip label="Live Data" color="primary" size="small" />
     </Tooltip>
   )
 }
@@ -18,7 +18,7 @@ export interface LiveDataDisclaimerProps {
 export function LiveDataDisclaimer({ cacheLifetime }: LiveDataDisclaimerProps) {
   return (
     <Typography variant="body2" color="text.secondary" fontStyle="italic">
-      Remote Data from this service may be outdated by up to <strong style={{ textWrap: 'nowrap' }}>{cacheLifetime.toLocaleString()}</strong> due to <a href="https://www.cloudflare.com/learning/cdn/what-is-caching/">caching</a>.
+      Live Data from this 3rd party service may be outdated by up to <strong style={{ textWrap: 'nowrap' }}>{cacheLifetime.toLocaleString()}</strong> due to <a href="https://www.cloudflare.com/learning/cdn/what-is-caching/">caching</a>.
       {/* <br/>
       This improves performance, reduces strain on 3rd party services, and makes CougarGrades more cost-effective to operate. */}
     </Typography>
--- lib/cache.ts
+++ lib/cache.ts
@@ -2,11 +2,13 @@ import { buildArgs } from './environment';
 import { days_to_seconds, hours_to_seconds, minutes_to_seconds } from './to_seconds'
 import { Temporal } from 'temporal-polyfill'
 
+export const PROD_CACHE_LIFETIME = Temporal.Duration.from({ days: 180 });
+
 /**
  * Server cache can be busted with a new commit (if the shape of the response changes),
  * but a browser's cache for HTTP responses can't be busted manually.
  */
-export const PROD_CACHE_CONTROL =    `public, must-revalidate, max-age=${hours_to_seconds(1)}, s-maxage=${days_to_seconds(180)}`;
+export const PROD_CACHE_CONTROL =    `public, must-revalidate, max-age=${hours_to_seconds(1)}, s-maxage=${PROD_CACHE_LIFETIME.total('seconds')}`;
 export const PREVIEW_CACHE_CONTROL = `public, must-revalidate, max-age=${minutes_to_seconds(5)}, s-maxage=${hours_to_seconds(1)}, stale-while-revalidate=${days_to_seconds(3)}`;
 export const LIVE_CACHE_CONTROL =    `public, must-revalidate, max-age=${15}, stale-while-revalidate=${minutes_to_seconds(60)}`;
 /**
@@ -20,6 +22,9 @@ export const TEMPORAL_CACHE_CONTROL = (serverDuration: Temporal.Duration, client
 }
 export const OFF_MAINTENANCE_CACHE_CONTROL = `public, must-revalidate, max-age=${0}, stale-while-revalidate=${minutes_to_seconds(5)}`;
 export const ON_MAINTENANCE_CACHE_CONTROL = `public, must-revalidate, max-age=${0}, stale-while-revalidate=${minutes_to_seconds(1)}`;
+export const LATEST_TERM_CACHE_LIFETIME = Temporal.Duration.from({ days: 1 });
+export const TRENDING_CACHE_LIFETIME = Temporal.Duration.from({ days: 7 });
+export const TOP_RECENT_CACHE_LIFETIME = Temporal.Duration.from({ days: 7 });
 
 const vercelEnv = buildArgs.vercelEnv
 export const CACHE_CONTROL = vercelEnv === 'preview' ? PREVIEW_CACHE_CONTROL : PROD_CACHE_CONTROL;
