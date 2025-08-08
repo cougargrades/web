b3f38de89e489024207311a28fdf712e8b899083
Austin Jackson <austinjckson@gmail.com>
2025-08-08 04:17:14 -0500

    fixed issue where zod parse failures would be fatal

--- lib/useTypedSearchParams.ts
+++ lib/useTypedSearchParams.ts
@@ -14,12 +14,23 @@ export type SetURLSearchParams<T extends z4.$ZodType<object>> = (
 /**
  * Wrapper around `useSearchParams` that validates with Zod.
  */
-export function useTypedSearchParams<T extends z4.$ZodType<{}>>(schema: T, defaultInit?: z4.output<T>): [z4.output<T>, SetURLSearchParams<T>]  {
+export function useTypedSearchParams<T extends z4.$ZodType<{}>>(schema: T, defaultInit: z4.output<T>): [z4.output<T>, SetURLSearchParams<T>]  {
   //const [searchParams, setSearchParams] = useSearchParams({ age: '99', foo: 'false' });
   const [searchParams, setSearchParams] = useSearchParams(defaultInit);
 
   // Parse the search params with zod
-  let parsedSearchParams = useMemo(() => z4.parse(schema, Object.fromEntries(searchParams.entries())), [searchParams]);
+  let parsedSearchParams = useMemo(() => {
+    /**
+     * Using `safeParse` is important because if you reroute to the root by clicking a link, then the
+     * searchParams will be empty "intentionally" for a moment (causing a parse failure), when
+     * normally "defaultInit" is only respected at the beginning.
+     * 
+     * This is our special behavior because we need everything to be type-safe. If this was an ordinary `useSearchParams`
+     * then we'd just get an empty URLSearchParams instance for 1 render cycle.
+     */
+    const parsed = z4.safeParse(schema, Object.fromEntries(searchParams.entries()));
+    return parsed.success ? parsed.data : defaultInit;
+  }, [searchParams]);
   let setTypedSearchParams = useCallback<SetURLSearchParams<T>>(
     (nextInit, navigateOptions) => {
       // TypeScript for the love of god, please...
