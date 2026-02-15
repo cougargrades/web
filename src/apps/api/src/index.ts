357c70a9451662c2de9aec1f1e59879a792be73b
Austin Jackson <austinjckson@gmail.com>
2026-02-14 19:00:22 -0600

    added cors to API

--- src/apps/api/src/index.ts
+++ src/apps/api/src/index.ts
@@ -1,9 +1,22 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
 
 import rmp from './routes/rmp'
 import simplesyllabus from './routes/simplesyllabus'
 
 const app = new Hono()
+
+app.use('*', cors({
+  origin: [
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "https://cougargrades.io",
+    "https://next.cougargrades.io",
+    "https://next2.cougargrades.io"
+  ],
+  allowMethods: ['POST', 'GET', 'OPTIONS'],
+  maxAge: 600,
+}))
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
