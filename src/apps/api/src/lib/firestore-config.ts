07025742d6e95bf44fc0deb9ebeb9f8bbd55241e
Austin Jackson <austinjckson@gmail.com>
2026-02-16 19:39:02 -0600

    issues using wrong secret

--- src/apps/api/src/lib/firestore-config.ts
+++ src/apps/api/src/lib/firestore-config.ts
@@ -3,6 +3,8 @@ import { env } from 'cloudflare:workers'
 import { FirestoreCredentials, firestore as _firestore } from '@cougargrades/vendor/firestore'
 
 export const GOOGLE_APPLICATION_CREDENTIALS = FirestoreCredentials.parse(JSON.parse(atob(env.GOOGLE_APPLICATION_CREDENTIALS)));
+
+console.debug(`🔥 Using Firestore ProjectID = '${GOOGLE_APPLICATION_CREDENTIALS.project_id}'`);
 // export const PROJECT_ID = GOOGLE_APPLICATION_CREDENTIALS.project_id;
 // export const DATABASE_ID = '(default)';
 
