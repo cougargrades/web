6220af308496f7de58cea50fe56802182bd66814
Austin Jackson <austinjckson@gmail.com>
2026-02-16 19:49:16 -0600

    ??????

--- src/apps/api/src/lib/firestore-config.ts
+++ src/apps/api/src/lib/firestore-config.ts
@@ -2,13 +2,13 @@
 import { env } from 'cloudflare:workers'
 import { FirestoreCredentials, firestore as _firestore } from '@cougargrades/vendor/firestore'
 
-export const GOOGLE_APPLICATION_CREDENTIALS = FirestoreCredentials.parse(JSON.parse(atob(env.GOOGLE_APPLICATION_CREDENTIALS)));
+// export const GOOGLE_APPLICATION_CREDENTIALS = FirestoreCredentials.parse(JSON.parse(atob(env.GOOGLE_APPLICATION_CREDENTIALS)));
 
-console.log(`🔥 Using Firestore ProjectID = '${GOOGLE_APPLICATION_CREDENTIALS.project_id}'`);
-// export const PROJECT_ID = GOOGLE_APPLICATION_CREDENTIALS.project_id;
-// export const DATABASE_ID = '(default)';
+//console.log(`🔥 Using Firestore ProjectID = '${GOOGLE_APPLICATION_CREDENTIALS.project_id}'`);
+
 
 export const firestore = () => {
+  const GOOGLE_APPLICATION_CREDENTIALS = FirestoreCredentials.parse(JSON.parse(atob(env.GOOGLE_APPLICATION_CREDENTIALS)));
   return _firestore(GOOGLE_APPLICATION_CREDENTIALS);
 }
 export const db = firestore();
