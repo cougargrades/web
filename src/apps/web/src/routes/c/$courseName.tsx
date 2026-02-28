88ec6e91b6b3fc35a5fad603944ba7a851deb67c
Austin Jackson <austinjckson@gmail.com>
2026-02-28 04:15:24 -0600

    improved handling of null values on /c/$courseName

--- src/apps/web/src/routes/c/$courseName.tsx
+++ src/apps/web/src/routes/c/$courseName.tsx
@@ -3,6 +3,7 @@ import { Alert, AlertTitle, Box, Chip, Container, Grid, Skeleton, Tooltip } from
 import { metaCourseDescription } from '@cougargrades/models';
 import type { SectionPlus } from '@cougargrades/models/dto';
 import { isNullish } from '@cougargrades/utils/nullish';
+import { defaultComparator } from '@cougargrades/utils/comparator';
 import Tilty from 'react-tilty'
 import { Chart } from 'react-google-charts'
 import { courseDataQueryOptions, useCourseData } from '../../lib/services/useCourseData'
@@ -23,23 +24,19 @@ import { SimpleSyllabusLauncher } from '../../components/SimpleSyllabus';
 
 import styles from './course.module.scss'
 import interactivity from '../../styles/interactivity.module.scss'
-import { defaultComparator } from '@cougargrades/utils/comparator';
 
 
 export const Route = createFileRoute('/c/$courseName')({
-  loader(ctx) {
+  async loader(ctx) {
     const queryOptions = courseDataQueryOptions(ctx.params.courseName);
-    ctx.context.queryClient.ensureQueryData(queryOptions).then(data => {
-      if (isNullish(data)) {
-        throw notFound();
-      }
-    });
-    return ctx.context.queryClient;
+    const data = await ctx.context.queryClient.ensureQueryData(queryOptions);
+    if (isNullish(data)) {
+      throw notFound();
+    }
+    return data;
   },
-  head: async (ctx) => {
-    const queryClient = ctx.loaderData;
-    const queryOptions = courseDataQueryOptions(ctx.params.courseName);
-    const data = await queryClient?.ensureQueryData(queryOptions);
+  head: (ctx) => {
+    const data = ctx.loaderData
     const metaDescription = metaCourseDescription({
       ...(data ?? {})
     })
@@ -60,16 +57,17 @@ function RouteComponent() {
 
   const { data, status, isPending } = useCourseData(courseName)
   const { data: sylData, status: sylStatus } = useSearchSimpleSyllabus(courseName)
+  const DID_LOAD = status === 'success' && !isNullish(data);
 
   const staticHTML = (
-    !isNullish(data) && data.publications.length > 0
+    !isNullish(data) && !isNullish(data.publications) && data.publications.length > 0
     ? data.publications.toSorted((a,b) => defaultComparator(a.catoid, b.catoid))[0].content
     : null
   );
 
   const RELATED_INSTRUCTOR_LIMIT = 4;
   const tccnsUpdateAsterisk = (
-    status === 'success' && data!.tccnsUpdates.length > 0
+    status === 'success' && !isNullish(data) && data.tccnsUpdates.length > 0
     ? (
       <Tooltip arrow disableInteractive placement="right" title={`${courseName} has been involved in some course number changes by UH.`}>
         <span>*</span>
@@ -98,7 +96,7 @@ function RouteComponent() {
             { !isPending ? <h3>{data?.description ?? ''}</h3> : <CustomSkeleton width={360} height={45} /> }
             { !isPending ? <h1 className={styles.display_3}>{courseName}{tccnsUpdateAsterisk}</h1> : <CustomSkeleton width={325} height={75} />}
             <div>
-              {status === 'success' ? data!.badges.map(e => (
+              {status === 'success' ? data?.badges.map(e => (
                 <Tooltip key={e.key} title={e.caption ?? ''}>
                   <Box component="span">
                     <Badge style={{ backgroundColor: e.color, marginRight: '0.35rem' }}>{e.text}</Badge>
@@ -114,7 +112,7 @@ function RouteComponent() {
           {
             status === 'success'
             ? (
-              data.tccnsUpdates.length > 0
+              !isNullish(data) && data.tccnsUpdates.length > 0
               ? data.tccnsUpdates.map((value, index) => (
                 <TCCNSUpdateNotice key={index} data={value} />
               ))
@@ -138,25 +136,36 @@ function RouteComponent() {
           <CustomSkeleton width={'100%'} height={125} />
          }
         { status === 'success' && data?.publications.length === 0 ? null : <h6>Sources:</h6> }
-        { status === 'success' ? data!.publications.map(e => (
+        { status === 'success' ? data?.publications.map(e => (
           <Tooltip key={e.key} title={`Scraped on ${new Date(e.scrapeDate).toLocaleString()}`}>
             <Chip label={e.title} className={`${styles.chip} ${interactivity.hoverActive}`} component="a" href={e.url} clickable />
           </Tooltip>
         )) : [1,2].map(e => <CustomSkeleton key={e} width={230} height={32} display="inline-block" />)}
         { status === 'success' ? <>
-          <EnrollmentInfo className={styles.enrollmentBar} data={data!.enrollment} barHeight={12} />          
+          <EnrollmentInfo className={styles.enrollmentBar} data={data?.enrollment ?? []} barHeight={12} />          
         </> : <CustomSkeleton width={'100%'} height={12} margin={0} /> }
         <Grid container spacing={0}>
           <Grid size={{ xs: 12, md: 6 }}>
             <h3 style={{ width: '90%' }}>Basic Information</h3>
             <ul>
-              <li>Earliest record: { status === 'success' ? data!.firstTaught : <Skeleton variant="text" style={{ display: 'inline-block' }} width={80} height={25} /> }</li>
-              <li>Latest record: { status === 'success' ? data!.lastTaught : <Skeleton variant="text" style={{ display: 'inline-block' }} width={80} height={25} /> }</li>
-              <li>Number of instructors: { status === 'success' ? data!.instructorCount : <Skeleton variant="text" style={{ display: 'inline-block' }} width={80} height={25} /> }</li>
-              <li>Number of sections: { status === 'success' ? data!.sectionCount : <Skeleton variant="text" style={{ display: 'inline-block' }} width={80} height={25} /> }</li>
+              <li>Earliest record: { status === 'success' ? data?.firstTaught : <Skeleton variant="text" style={{ display: 'inline-block' }} width={80} height={25} /> }</li>
+              <li>Latest record: { status === 'success' ? data?.lastTaught : <Skeleton variant="text" style={{ display: 'inline-block' }} width={80} height={25} /> }</li>
+              <li>Number of instructors: { status === 'success' ? data?.instructorCount : <Skeleton variant="text" style={{ display: 'inline-block' }} width={80} height={25} /> }</li>
+              <li>Number of sections: { status === 'success' ? data?.sectionCount : <Skeleton variant="text" style={{ display: 'inline-block' }} width={80} height={25} /> }</li>
               <li>
                 <Tooltip arrow disableInteractive title={`Estimated average size of each section, # of total enrolled ÷ # of sections. Excludes "empty" sections.`}>
-                  <span>Average number of students per section: { status === 'success' ? data!.classSize < 0 ? 'N/A' : `~ ${data?.classSize.toFixed(1)}` : <Skeleton variant="text" style={{ display: 'inline-block' }} width={80} height={25} /> }</span>
+                  <span>
+                    Average number of students per section:{' '} 
+                    {
+                      status === 'success'
+                      ? (
+                        isNullish(data) || data.classSize < 0
+                        ? 'N/A'
+                        : `~ ${data?.classSize.toFixed(1)}`
+                      )
+                      : <Skeleton variant="text" style={{ display: 'inline-block' }} width={80} height={25} />
+                      }
+                  </span>
                 </Tooltip>
               </li>
             </ul>
@@ -164,19 +173,19 @@ function RouteComponent() {
           <Grid size={{ xs: 12, md: 6 }}>
             <h3 style={{ width: '90%' }}>Seasonal Availability</h3>
             {
-              status === 'success'
-              ? <SeasonalAvailabilityInfo style={{ paddingTop: '10px' }} seasonalAvailability={data!.seasonalAvailability} />
+              DID_LOAD
+              ? <SeasonalAvailabilityInfo style={{ paddingTop: '10px' }} seasonalAvailability={data.seasonalAvailability} />
               : <CustomSkeleton width={150} height={150} margin={0} />
             }
           </Grid>
         </Grid>
         <h3>Related Groups</h3>
         {
-          status === 'success' 
+          DID_LOAD
           ? (
-              data!.relatedGroups.length > 0
+              data.relatedGroups.length > 0
               ? (
-                data!.relatedGroups.map(e => (
+                data.relatedGroups.map(e => (
                   <Tooltip key={e.key} title={e.description}>
                     <Chip label={e.title} className={`${styles.chip} ${interactivity.hoverActive}`} component="a" href={e.href} clickable />
                   </Tooltip>
@@ -192,22 +201,22 @@ function RouteComponent() {
         }
         <h3>Related Instructors</h3>
         {
-          status === 'success' 
+          DID_LOAD
           ? (
-            data!.relatedInstructors.length > 0
+            data.relatedInstructors.length > 0
             ? (
               <Carousel>
                 {
-                  data!.relatedInstructors
+                  data.relatedInstructors
                   .slice(0,RELATED_INSTRUCTOR_LIMIT)
                   .map(e => <Tilty key={e.key} max={25}><InstructorCard data={e} /></Tilty>)
                 }
                 {
-                  data!.relatedInstructors.length > RELATED_INSTRUCTOR_LIMIT
+                  data.relatedInstructors.length > RELATED_INSTRUCTOR_LIMIT
                   ? <InstructorCardShowMore
                       cardTitle={`View all ${courseName} instructors`}
                       modalTitle={`All ${courseName} instructors`}
-                      data={data!.relatedInstructors} 
+                      data={data.relatedInstructors} 
                     />
                   : null
                 }
@@ -249,15 +258,15 @@ function RouteComponent() {
         {
           status === 'success'
           ? (
-            data!.dataChart.data.length > 0
+            DID_LOAD && data.dataChart.data.length > 0
             ? (
               <Chart
                 width={'100%'}
                 height={450}
                 chartType="LineChart"
                 loader={<CustomSkeleton width={'100%'} height={350} />}
-                data={data!.dataChart.data}
-                options={data!.dataChart.options}
+                data={data.dataChart.data}
+                options={data.dataChart.options}
                 // prevent ugly red box when there's no data yet on first-mount
                 chartEvents={[{ eventName: 'error', callback: (event) => event.google.visualization.errors.removeError(event.eventArgs[0].id) }]}
               />
@@ -278,8 +287,8 @@ function RouteComponent() {
       <main>
         <EnhancedTable<SectionPlus>
           title="Past Sections"
-          columns={status === 'success' ? data!.dataGrid.columns : []}
-          rows={status === 'success' ? data!.dataGrid.rows : []}
+          columns={DID_LOAD ? data.dataGrid.columns : []}
+          rows={DID_LOAD ? data.dataGrid.rows : []}
           defaultOrder="desc"
           defaultOrderBy="term"
         />
@@ -303,6 +312,7 @@ function NotFoundComponent() {
             <AlertTitle>Error 404</AlertTitle>
             Course &quot;<code className="plain">{courseName}</code>&quot; could not be found.
           </Alert>
+          <div style={{ height: '200px' }}></div>
         </div>
       </main>
     </Container>
